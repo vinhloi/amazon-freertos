f21c2e54239c73e1f3b32341344146117d877d5e
NoMaY <NoMaY-jp@outlook.com>
2018-08-01 16:12:53 +0900

    Pre Phase 3, Addendum A : A workaround to avoid the problem that the BSP_CFG_RTOS_USED in the r_bsp_config.h is set to (0) every time of code generation by the Smart Configurator.

--- lib/third_party/mcu_vendor/renesas/FIT/RDP_v1.15_modified/ccrx/generic_rx65n/r_bsp/board/generic_rx65n/r_bsp.h
+++ lib/third_party/mcu_vendor/renesas/FIT/RDP_v1.15_modified/ccrx/generic_rx65n/r_bsp/board/generic_rx65n/r_bsp.h
@@ -41,6 +41,11 @@
 INCLUDE APPROPRIATE MCU AND BOARD FILES
 ***********************************************************************************************************************/
 #include    "mcu/all/r_bsp_common.h"
+/* As of today, we need a workaround to avoid the problem that the BSP_CFG_RTOS_USED in the r_bsp_config.h is
+ * set to (0) every time of code generation by the Smart Configurator. We add a workaround modification in the
+ * r_bsp_config.h which makes the setting of there ignored when the BSP_CFG_RTOS_USED is set to non-zero value
+ * already and we set the BSP_CFG_RTOS_USED to (1) here so that the setting in the r_bsp_config.h is ignored. */
+#define BSP_CFG_RTOS_USED             (1)
 #include    "r_bsp_config.h"
 #include    "r_bsp_interrupt_config.h"
 #include    "mcu/rx65n/register_access/iodefine.h"
