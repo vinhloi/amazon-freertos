a027ab1222c2a8dc74f8a3468b6045ab3c458e24
NoMaY <NoMaY-jp@outlook.com>
2018-08-16 01:37:10 +0900

    Remove some compile warning messages caused by the workaround regarding to the BSP_CFG_RTOS_USED at the commit f21c2e5.

--- lib/third_party/mcu_vendor/renesas/FIT/RDP_v1.15_modified/gnurx/generic_rx65n/r_bsp/board/generic_rx65n/r_bsp.h
+++ lib/third_party/mcu_vendor/renesas/FIT/RDP_v1.15_modified/gnurx/generic_rx65n/r_bsp/board/generic_rx65n/r_bsp.h
@@ -45,6 +45,9 @@ INCLUDE APPROPRIATE MCU AND BOARD FILES
  * set to (0) every time of code generation by the Smart Configurator. We add a workaround modification in the
  * r_bsp_config.h which makes the setting of there ignored when the BSP_CFG_RTOS_USED is set to non-zero value
  * already and we set the BSP_CFG_RTOS_USED to (1) here so that the setting in the r_bsp_config.h is ignored. */
+#if defined(BSP_CFG_RTOS_USED)
+#undef BSP_CFG_RTOS_USED
+#endif
 #define BSP_CFG_RTOS_USED             (1)
 #include    "r_bsp_config.h"
 #include    "r_bsp_interrupt_config.h"
