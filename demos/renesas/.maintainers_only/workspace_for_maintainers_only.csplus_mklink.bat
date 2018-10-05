37c0066f58a83bbe02004f41efaf301d0be9f064
NoMaY <NoMaY-jp@outlook.com>
2018-10-05 17:34:06 +0900

    Add a CC-RX/CS+ workspace 'workspace_for_maintainers_only.mtpj' which contains all CC-RX/CS+ projects by including sub project files (.mtsp files) and add a batch file which creates symbolic links from .mtpj file to .mtsp file by executing the MKLINK command of the Windows.

---
+++ demos/renesas/.maintainers_only/workspace_for_maintainers_only.csplus_mklink.bat
@@ -0,0 +1,9 @@
+cd /d "%~dp0"
+mklink ..\rx63n-gr-sakura\ccrx-csplus\aws_demos.RX63N.GR-SAKURA.CC-RX.mtsp aws_demos.RX63N.GR-SAKURA.CC-RX.mtpj
+mklink ..\rx64m-gr-kaede\ccrx-csplus\aws_demos.RX64M.GR-KAEDE.CC-RX.mtsp aws_demos.RX64M.GR-KAEDE.CC-RX.mtpj
+mklink ..\rx65n-envision-kit\ccrx-csplus\aws_demos.RX65N.Envision-Kit.CC-RX.mtsp aws_demos.RX65N.Envision-Kit.CC-RX.mtpj
+mklink ..\rx65n-gr-rose\ccrx-csplus\aws_demos.RX65N.GR-ROSE.CC-RX.mtsp aws_demos.RX65N.GR-ROSE.CC-RX.mtpj
+mklink ..\rx65n-rsk\ccrx-csplus\aws_demos.RX65N.RSK.CC-RX.mtsp aws_demos.RX65N.RSK.CC-RX.mtpj
+mklink ..\rx65n-rsk-uart-sx-ulpgn\ccrx-csplus\aws_demos.RX65N.RSK.UART.SX-ULPGN.CC-RX.mtsp aws_demos.RX65N.RSK.UART.SX-ULPGN.CC-RX.mtpj
+mklink ..\rx65n-tb-uart-sx-ulpgn\ccrx-csplus\aws_demos.RX65N.TB.UART.SX-ULPGN.CC-RX.mtsp aws_demos.RX65N.TB.UART.SX-ULPGN.CC-RX.mtpj
+pause
