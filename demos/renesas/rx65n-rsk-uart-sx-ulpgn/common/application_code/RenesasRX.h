7229838df1995bd212325315614e7399b588ba2a
NoMaY <NoMaY-jp@outlook.com>
2018-11-24 08:43:33 +0900

    Introduce RenesasRX.h (from a marketing view point, this header file name might be good).

---
+++ demos/renesas/rx65n-rsk-uart-sx-ulpgn/common/application_code/RenesasRX.h
@@ -0,0 +1,24 @@
+#ifndef _RENESASRX_H_
+#define _RENESASRX_H_
+
+#include "platform.h"
+
+//#include "r_ether_rx_if.h"
+#include "r_flash_rx_if.h"
+#include "r_byteq_if.h"
+#include "r_sci_rx_if.h"
+//#include "r_cmt_rx_if.h"
+//#include "r_riic_rx_if.h"
+//#include "r_sci_iic_rx_if.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "r_smc_entry.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RENESASRX_H_ */
