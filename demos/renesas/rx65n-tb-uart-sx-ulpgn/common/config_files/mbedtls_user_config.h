ec1321aec7145387d409895c7a6627a9714371ed
NoMaY <NoMaY-jp@outlook.com>
2018-12-04 00:30:15 +0900

    Refactor lib/third_party/mbedtls/library/platform_util.c to remove __CCRX__ and __RX which were added in the commit 71c9ec3.

--- demos/renesas/rx65n-tb-uart-sx-ulpgn/common/config_files/mbedtls_user_config.h
+++ demos/renesas/rx65n-tb-uart-sx-ulpgn/common/config_files/mbedtls_user_config.h
@@ -8,3 +8,13 @@
 #if defined(MBEDTLS_SELF_TEST)
 #undef MBEDTLS_SELF_TEST
 #endif
+
+/* Workaround for CC-RX's non-standard 2nd argument type of 
+ * memset function. CC-RX uses long but standard uses int.
+ */
+#if defined(__CCRX__)
+#include "string.h"
+typedef void * (* mbedtls_platform_zeroize_memset_t)( void *, int, size_t );
+#undef memset
+#define memset ((mbedtls_platform_zeroize_memset_t)memset)
+#endif
