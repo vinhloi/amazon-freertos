62b6aff0eff79b61fafdeee18b9675bf4a42a896
NoMaY <NoMaY-jp@outlook.com>
2018-12-13 11:45:52 +0900

    Revert tinycbor's math_support_p.h to the upstream's file by moving the change of the commit f243731 from math_support_p.h to implicitlyinclude.h for CC-RX. (This (and also previous 96db6b8) is a no risk commit for the RX65N RSK AFQP TEST project because its assembler code by CC-RX are not changed, i.e. its MOT file does not change.)

--- lib/third_party/mcu_vendor/renesas/amazon_freertos_common/compiler_support/ccrx/implicitlyinclude.h
+++ lib/third_party/mcu_vendor/renesas/amazon_freertos_common/compiler_support/ccrx/implicitlyinclude.h
@@ -1,1 +1,10 @@
 #define MBEDTLS_USER_CONFIG_FILE "mbedtls_user_config.h"
+
+/* Avoid CC-RX's compiler warning message 'M0520193: Zero used for 
+ * undefined preprocessing identifier "XXXX"' in CC-RX's math.h */
+#if !defined(_FEVAL)
+#define _FEVAL 0
+#endif
+#if !defined(_HAS_C9X_FAST_FMA)
+#define _HAS_C9X_FAST_FMA 0
+#endif
