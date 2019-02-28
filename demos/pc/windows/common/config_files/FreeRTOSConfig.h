03a8b737727268fa93fc50457c1e7e9b14033bd8
vinhloi <vinh107108@gmail.com>
2019-02-28 11:04:01 +0700

    just for debugging

--- demos/pc/windows/common/config_files/FreeRTOSConfig.h
+++ demos/pc/windows/common/config_files/FreeRTOSConfig.h
@@ -161,7 +161,7 @@ extern void vLoggingPrintf( const char * pcFormat,
  * results in the wired network being used, while setting
  * configNETWORK_INTERFACE_TO_USE to 2 results in the wireless network being
  * used. */
-#define configNETWORK_INTERFACE_TO_USE       ( 0L )
+#define configNETWORK_INTERFACE_TO_USE       ( 1L )
 
 /* The address of an echo server that will be used by the two demo echo client
  * tasks:
